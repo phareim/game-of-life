673b504ef40505cdd4545bf9ac8f79c19a41aad4
Petter Hareim <phareim@gmail.com>
2014-07-25 08:15:47 +0200

    trying to fix heroku

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var app = express();
 
 var port = Number(process.env.PORT || 5000);
 
+app.use('/', express.static(__dirname + '/'));
 app.use('/', express.static(__dirname + '/public'));
 
 app.listen(port, function() {
